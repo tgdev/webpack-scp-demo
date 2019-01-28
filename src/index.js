d1b7cf375405ac5618a8a420eb263b88fb0978ef
Tom Gillard <tomgillar@gmail.com>
2019-01-28 17:14:23 +1100

    Add page components and app styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, NavLink as Link, Route } from 'react-router-dom';
+
+import './styles.scss';
+
+// home route component
+const HomeComponent = (props) => {
+    return (
+        <h1>Home Component!</h1>
+    );
+}
+
+// about route component
+const AboutComponent = (props) => {
+    return (
+        <h1>About Component!</h1>
+    );
+}
+
+// contact route component
+const ContactComponent = (props) => {
+    return (
+        <h1>Contact Component!</h1>
+    );
+}
 
 // create sample App component
 class App extends React.Component {
     constructor(props) {
         super(props);
     }
+
     render() {
         return (
-            <h1>Hello React!</h1>
+            <BrowserRouter>
+                <div>
+                    <div className="menu">
+                        <Link exact to="/" activeClassName="active">Home</Link>
+                        <Link to="/about" activeClassName="active">About</Link>
+                        <Link to="/contact" activeClassName="active">Contact</Link>
+                    </div>
+
+                    <Switch>
+                        <Route exact path="/" component={HomeComponent} />
+                        <Route path="/about" component={AboutComponent} />
+                        <Route path="/contact" component={ContactComponent} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         );
     }
 }
