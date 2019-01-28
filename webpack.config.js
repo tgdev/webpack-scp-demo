8e6f33c2cfc28092430e22650d60ae72d8758395
Tom Gillard <tomgillar@gmail.com>
2019-01-28 18:15:05 +1100

    Add vendor chuck and fix chunk names

--- webpack.config.js
+++ webpack.config.js
@@ -6,50 +6,77 @@ const config = require('config');
 /*-------------------------------------------------*/
 
 module.exports = {
-    // webpack optimization mode
-    mode: ( process.env.NODE_ENV ? process.env.NODE_ENV : 'development' ),
+  // webpack optimization mode
+  mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
 
-    // entry file(s)
-    entry: './src/index.js',
+  // entry file(s)
+  entry: './src/index.js',
 
-    // output file(s) and chunks
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: '/'
-    },
+  // output file(s) and chunks
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    chunkFilename: '[name].js',
+    publicPath: config.get('publicPath')
+  },
 
-    // module/loaders configuration
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-            }
-        ]
-    },
+  // bundle optimisation
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
 
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'index.html')
-        })
-    ],
+        // vendor chunk
+        vendor: {
+          name: 'vendor',
+          // sync + async chunks
+          chunks: 'all',
+          // import file path containing node_modules
+          test: /node_modules/
+        }
+      }
+    }
+  },
 
-    // development server configuration
-    devServer: {
-        
-        // must be `true` for SPAs
-        historyApiFallback: true,
+  // module/loaders configuration
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      }
+    ]
+  },
 
-        // open browser on server start
-        open: config.get('open')
-    },
+  plugins: [
+    new webpack.NamedChunksPlugin(
+      chunk =>
+        chunk.name ||
+        chunk.mapModules(m => path.basename(m.request, '.js')).join('_')
+    ),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ],
 
-    // generate source map
-    devtool: (process.env.NODE_ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map' ),
+  // development server configuration
+  devServer: {
+    // must be `true` for SPAs
+    historyApiFallback: true,
+
+    // open browser on server start
+    open: config.get('open')
+  },
+
+  // generate source map
+  devtool:
+    process.env.NODE_ENV === 'production'
+      ? 'source-map'
+      : 'cheap-module-eval-source-map'
 };
