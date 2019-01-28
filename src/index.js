90aa9712afb8c664ef08dfe30af513fad581c867
Tom Gillard <tomgillar@gmail.com>
2019-01-28 17:12:15 +1100

    Add main App component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Chunking demo with Webpack 4');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create sample App component
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>Hello React!</h1>
+        );
+    }
+}
+
+// render inside `app-root` element
+ReactDOM.render(<App />, document.getElementById('app-root'));
